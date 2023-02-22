8e5d2afeb41131fd2ad6a36148009894eaac54b8
harshit shukla <hharshit8118@gmail.com>
2023-02-23 01:32:05 +0530

    add range update point query function

---
+++ SegmentTree/rangeupdatePointquery.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// #ifndef ONLINE_JUDGE     // #include "C://Users//hhars//OneDrive//Documents//CPP Practice//Algo//config.h"
+// #include "Algo/debug.h"   // include definitions
+// #else
+// #define fix(x)
+// #endif
+
+vector<int> arr;
+class segmentTree{
+  public: 
+  vector<int> t;
+  segmentTree(int n){
+    t.resize(4*n);
+  }
+  template<class T> T operation(T a, T b){ return a + b; } 
+  template<class T>  void build(T v, T tl, T tr){
+    if(tl == tr){
+      t[v] = arr[tl];      return;
+    }T tm = (tl + tr)/2;
+    build(v*2, tl, tm);     build(v*2+1, tm+1, tr);
+    t[v] = 0;
+  }
+  template<class T>  void update(T v, T tl, T tr, T l, T r, T val){
+    if(l > r) return;
+    if(tl == l and tr == r){
+      t[v] += val; return;
+    }T tm = (tl + tr)/2;
+    update(v*2, tl, tm, l, min(r, tm), val);
+    update(v*2+1, tm+1, tr, max(l, tm+1), r, val);
+  }
+  template<class T>  T get_query(T v, T tl, T tr, T pos){
+    if(tl == tr)      return t[v];
+    T tm = (tl + tr)/2;
+    if(tm >= pos)
+      return t[v] + get_query(v*2, tl, tm, pos);
+    else
+      return t[v] + get_query(v*2+1, tm+1, tr, pos);
+  }
+};
+
+
+int main(){
+
+ios_base::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+// #ifndef ONLINE_JUDGE
+//     #include "Algo/config.h"
+// #endif
+
+int n;cin>>n;
+segmentTree obj(n);
+arr = vector<int>(n);
+for(auto &x: arr) cin>>x;
+obj.build(1, 0, n-1);
+// fix(obj.t);
+obj.update(1, 0, n-1, 2, 4, 3);
+obj.update(1, 0, n-1, 2, 4, -3);
+obj.update(1, 0, n-1, 2, 4, 2);
+// fix(obj.t);
+cout<<obj.get_query(1, 0, n-1, 2);
+
+return 0;
+}
